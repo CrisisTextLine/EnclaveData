1fb2d97a5f82640cabfb0b220e5e68f263d9b859
Chris Johnson <chris@cjcodes.com>
2017-03-15 21:27:23 -0400

    Migrate to the new researchers structure

---
+++ ansible/migrate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd credentials
+mv app-state app-state.jennifer
+mv basel basel.miriam
+mv berkeley berkeley.adrian
+mv brain brain.rebekah
+mv cornell cornell.cristian
+mv kempe kempe.laura
+mv montana montana.ryan
+mv northwestern northwestern.james
+mv ryerson ryerson.eric
+mv stanford-cs stanford-cs.jure
+mv stanford-ess stanford-ess.marshall
+mv ut-austin ut-austin.hannah
+mv virginia-tech virginia-tech.stephen
